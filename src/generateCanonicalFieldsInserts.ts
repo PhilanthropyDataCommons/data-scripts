ba13a1d4b92319cc72f0d96e1c5fa888bb56088d
gmurtaza00 <111306331+gmurtaza00@users.noreply.github.com>
2023-02-28 08:27:52 -0500

    Updated script according to new format of Canonical Fields
    * Updated script according to new format Canonical Fields introduced in January
    
    * Added comments and changed if condition to check for single quotes instead of double qoutes
    
    * Read data by column names rather than indexes
    
    * Comments adjusted

--- src/generateCanonicalFieldsInserts.ts
+++ src/generateCanonicalFieldsInserts.ts
@@ -1,5 +1,5 @@
-// Takes a pipe-separated values file and creates a canonical fields INSERT SQL file.
-import fs, { WriteStream } from 'fs';
+// Takes a comma-separated values file and creates a canonical fields INSERT SQL file.
+import fs from 'fs';
 import os from 'os';
 import CsvReadableStream from 'csv-reader';
 import { parse } from 'ts-command-line-args';
@@ -14,30 +14,31 @@ const args = parse<Args>({
   outputFile: String,
 });
 
-const psvInput = fs.createReadStream(args.inputFile, 'utf8');
+const csvInput = fs.createReadStream(args.inputFile, 'utf8');
 const sqlOutput = fs.createWriteStream(args.outputFile, 'utf8');
 
-sqlOutput.write(`INSERT INTO canonical_fields (id, label, short_code, data_type, created_at) OVERRIDING SYSTEM VALUE VALUES${os.EOL}`);
+sqlOutput.write(`INSERT INTO canonical_fields (id, label, short_code, data_type) OVERRIDING SYSTEM VALUE VALUES${os.EOL}`);
 
 let firstRowArrived = false;
-psvInput.pipe(
+csvInput.pipe(
   new CsvReadableStream({
     parseNumbers: true,
     parseBooleans: true,
     trim: true,
-    delimiter: '|',
+    allowQuotes: true,
     asObject: true,
   }),
 ).on('data', (row: any) => {
-  const label = row.label.replace('\'', '\'\'');
-  const shortCode = row.shortCode.replace('\'', '\'\'');
-  const dataType = row.dataType.replace('\'', '\'\'');
-  const createdAt = row.createdAt.replace('\'', '\'\'');
+  const rowID = row['ID'];
+  const label = row['Label'];
+  const shortCode = row['Internal field name'];
+  const dataType = row['Type'];
+
   if (firstRowArrived) {
-    sqlOutput.write(`,${os.EOL}(${row.id}, '${label}', '${shortCode}', '${dataType}', '${createdAt}')`);
+    sqlOutput.write(`,${os.EOL}(${rowID}, '${label}' , '${shortCode}', '${dataType}' )`);
   }
   else {
-    sqlOutput.write(`(${row.id}, '${label}', '${shortCode}', '${dataType}', '${createdAt}')`);
+    sqlOutput.write(`(${rowID}, '${label}', '${shortCode}', '${dataType}' )`);
   }
   firstRowArrived = true;
 }).on('end', () => {
