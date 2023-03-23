b8b33aabfafe17a981740bfeb52542243c30bf1a
gmurtaza00 <111306331+gmurtaza00@users.noreply.github.com>
2023-03-23 12:37:33 -0400

    Script updated to create application form json data (#3)
    * Script updated to create application form json data
    
    * Adjusted script to cater empty position fields and changed the command line argument from authHeader to bearerToken
    
    * Remove .env file,dotenv package and extra spaces around if condition
    
    * Added new argument "--apiUrl", url should be without trailing /

--- src/generateApplicationFormJson.ts
+++ src/generateApplicationFormJson.ts
@@ -1,14 +1,21 @@
-// Takes a pipe-separated values file and creates a JSON body for POST /applicationForms.
+// Takes a csv file and creates a JSON body for POST /applicationForms.
 import fs from 'fs';
 import CsvReadableStream from 'csv-reader';
 import { parse } from 'ts-command-line-args';
-
+import  axios  from 'axios'
+import { AxiosError } from 'axios';
 interface Args {
   inputFile: string;
   outputFile: string;
   opportunityId: number;
+  funder: string;
+  bearerToken: string;
+  apiUrl: string;
 }
 
+interface csvRow {
+  [key: string]: any;
+}
 interface ApplicationFormField {
   canonicalFieldId: number;
   position: number;
@@ -19,41 +26,68 @@ interface ApplicationForm {
   fields: ApplicationFormField[];
 }
 
-interface SpreadsheetAdditionalColumns {
-  funderField: string;
+interface CanonicalField {
+  id: number;
+  label: string;
+  shortCode: string;
+  dataType: string;
+  createdAt: string;
 }
 
 const args = parse<Args>({
   inputFile: String,
   outputFile: String,
   opportunityId: Number,
+  funder: String,
+  bearerToken: String,
+  apiUrl: String
 });
 
-const psvInput = fs.createReadStream(args.inputFile, 'utf8');
+const csvInput = fs.createReadStream(args.inputFile, 'utf8');
 const jsonOutput = fs.createWriteStream(args.outputFile, 'utf8');
-const { opportunityId } = args;
+const { opportunityId, funder, bearerToken, apiUrl } = args;
 
 let applicationForm: ApplicationForm = {
     opportunityId,
     fields: [],
 }
+let counter = 0;
 
-psvInput.pipe(
-  new CsvReadableStream({
-    parseNumbers: true,
-    parseBooleans: true,
-    trim: true,
-    delimiter: '|',
-    asObject: true,
-  }),
-).on('data', (row: ApplicationFormField & SpreadsheetAdditionalColumns) => {
-  const applicationFormField: ApplicationFormField = {
-    canonicalFieldId: row.canonicalFieldId,
-    position: row.position,
-    label: row.label,
+axios(apiUrl+'/canonicalFields',{
+  'method': 'GET',
+  'headers' : {
+    'accept': 'application/json',
+    'Authorization': 'Bearer ' + bearerToken
   }
-  applicationForm.fields.push(applicationFormField);
-}).on('end', () => {
-  jsonOutput.write(JSON.stringify(applicationForm));
-  jsonOutput.close();
+}).then((response) => {
+  let fields: CanonicalField[] = response.data;
+  csvInput.pipe(
+    new CsvReadableStream({
+      parseNumbers: true,
+      parseBooleans: true,
+      trim: true,
+      allowQuotes: true,
+      asObject: true,
+    }),
+  ).on('data', (row: csvRow) => {
+    const label = funder + ': field label';
+    const id = funder + ': external ID';
+    const pos = funder + ': form position';
+    let field: CanonicalField[] | any;
+    if (row[id] !== '') {
+      const shortCode = row['Internal field name'];
+      field = fields.filter(e  => e['shortCode'] === shortCode);
+      const applicationFormField: ApplicationFormField = {
+        canonicalFieldId: field[0].id,
+        position: row[pos] === '' ? counter++ : row[pos],
+        label: row[label],
+      }
+      applicationForm.fields.push(applicationFormField);
+    }
+  }).on('end', () => {
+    jsonOutput.write(JSON.stringify(applicationForm));
+    jsonOutput.close();
+  });
+}).catch((error: AxiosError) => {
+  console.log(error.response?.data)
 });
