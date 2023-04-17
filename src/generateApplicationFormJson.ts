7e19e6cdeda16c66f6ce31ba817f5d069b81ce2b
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2023-04-17 14:12:13 -0400

    Use templated strings instead of string concatenation. Improved interface of ApplicationFormField. Changed CsvRow type key/value pairs from <string>:<undefined> to <string>:<string> and use find instead of filter to find a specific canonical field from all canonical fields

--- src/generateApplicationFormJson.ts
+++ src/generateApplicationFormJson.ts
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import CsvReadableStream from 'csv-reader';
 import { parse } from 'ts-command-line-args';
-import axios, { AxiosError } from 'axios'
+import axios, { AxiosError } from 'axios';
 
 interface Args {
   inputFile: string;
@@ -13,11 +13,11 @@ interface Args {
   apiUrl: string;
 }
 
-type csvRow = Record<string, any>;
+type CsvRow = Record<string, string>;
 interface ApplicationFormField {
   canonicalFieldId: number;
-  position: number;
-  label: string;
+  position: number | string | undefined;
+  label: string | undefined;
 }
 interface ApplicationForm {
   opportunityId: number;
@@ -47,20 +47,20 @@ const {
   opportunityId, funder, bearerToken, apiUrl,
 } = args;
 
-let applicationForm: ApplicationForm = {
+const applicationForm: ApplicationForm = {
   opportunityId,
   fields: [],
-}
+};
 let counter = 0;
 
-axios(apiUrl + '/canonicalFields', {
+axios(`${apiUrl}/canonicalFields`, {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer ' + bearerToken,
+    Authorization: `Bearer ${bearerToken}`,
   },
 }).then((response) => {
-  let fields: CanonicalField[] = response.data;
+  const fields: CanonicalField[] = response.data as CanonicalField[];
   csvInput.pipe(
     new CsvReadableStream({
       parseNumbers: true,
@@ -69,25 +69,27 @@ axios(apiUrl + '/canonicalFields', {
       allowQuotes: true,
       asObject: true,
     }),
-  ).on('data', (row: csvRow) => {
-    const label = funder + ': field label';
-    const id = funder + ': external ID';
-    const pos = funder + ': form position';
-    let field: CanonicalField[] | any;
+  ).on('data', (row: CsvRow) => {
+    const label = `${funder}: field label`;
+    const id = `${funder}: external ID`;
+    const pos = `${funder}: form position`;
+    let field: CanonicalField | undefined;
     if (row[id] !== '') {
       const shortCode = row['Internal field name'];
-      field = fields.filter((e) => e['shortCode'] === shortCode);
-      const applicationFormField: ApplicationFormField = {
-        canonicalFieldId: field[0].id,
-        position: row[pos] === '' ? counter++ : row[pos],
-        label: row[label],
+      field = fields.find((e) => e.shortCode === shortCode);
+      if (field) {
+        const applicationFormField: ApplicationFormField = {
+          canonicalFieldId: field.id,
+          position: row[pos] ? '' : (counter += 1),
+          label: row[label],
+        };
+        applicationForm.fields.push(applicationFormField);
       }
-      applicationForm.fields.push(applicationFormField);
     }
   }).on('end', () => {
     jsonOutput.write(JSON.stringify(applicationForm));
     jsonOutput.close();
   });
 }).catch((error: AxiosError) => {
-  console.log(error.response?.data)
+  console.log(error.response?.data);
 });
