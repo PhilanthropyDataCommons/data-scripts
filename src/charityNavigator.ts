1a2afc555fe096141c8af6bffa411dd4b22ff752
Jesse Bickel <jbickel@opentechstrategies.com>
2025-12-17 15:14:45 -0600

    WIP Fix eslint errors

--- src/charityNavigator.ts
+++ src/charityNavigator.ts
@@ -1,11 +1,10 @@
-import { ApolloClient, DocumentNode, InMemoryCache } from '@apollo/client';
+import { writeFile } from 'fs/promises';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import { SetContextLink } from '@apollo/client/link/context';
 import { HttpLink } from '@apollo/client/link/http';
-import { gql } from '@apollo/client/core';
 import { isValidEin } from './ein';
 import { logger } from './logger';
 import type { CommandModule } from 'yargs';
-import type { AccessTokenSet } from './oidc';
 
 const queryNonprofitsPublic = gql`
   query NonprofitsPublic(
@@ -28,36 +27,32 @@ const queryNonprofitsPublic = gql`
   }
 `;
 
-
 function apolloInit(apiUrl: string, apiKey: string) {
   const cache = new InMemoryCache();
-  const authLink = new SetContextLink(({ headers }, _) => {
-    return {
-      headers: {
-        ...headers,
-        Authorization: apiKey,
-      },
-    };
-  });
+  const authLink = new SetContextLink(({ headers }) => ({
+    headers: {
+      ...headers,
+      Authorization: apiKey,
+    },
+  }));
 
   const httpLinkPrimary = new HttpLink({
     uri: `${apiUrl}`,
   });
   const apolloClient = new ApolloClient({
     link: authLink.concat(httpLinkPrimary),
-    cache: cache,
+    cache,
   });
 
   return apolloClient;
 }
-const API_URL = 'https://api.charitynavigator.org/graphql'
-
+const API_URL = 'https://api.charitynavigator.org/graphql';
 
 const getCharityNavigatorProfiles = async (
   apiKey: string,
   eins: string[],
-): Promise<ApolloClient.QueryResult<unknown>> => {
-  logger.debug(`Looking up EINs ${eins} in Charity Navigator GraphQL API`);
+): Promise<ApolloClient.QueryResult> => {
+  logger.debug(`Looking up EINs ${JSON.stringify(eins)} in Charity Navigator GraphQL API`);
   const apollo = apolloInit(API_URL, apiKey);
   const variables = {
     filter: {
@@ -68,17 +63,16 @@ const getCharityNavigatorProfiles = async (
     page: 1,
     resultSize: eins.length,
   };
-  logger.debug(`Fetching charity navigator data for ${eins} using vars ${variables}`);
+  logger.debug(`Fetching charity navigator data for ${JSON.stringify(eins)} using vars ${JSON.stringify(variables)}`);
   return apollo
-      .query({
-        query: queryNonprofitsPublic,
-        variables,
-      });
+    .query({
+      query: queryNonprofitsPublic,
+      variables,
+    });
 };
 
-
 interface LookupCommandArgs {
-  'charityNavigator-api-key': string;
+  'charity-navigator-api-key': string;
   eins: string[];
   outputFile?: string;
 }
@@ -107,9 +101,9 @@ const lookupCommand: CommandModule<unknown, LookupCommandArgs> = {
     .check(({ eins }) => !(new Set(eins.map(isValidEin)).has(false)))
   ),
   handler: async (args) => {
-    const result = await getCharityNavigatorProfiles(args['charityNavigatorApiKey'], args.eins)
+    const result = await getCharityNavigatorProfiles(args.charityNavigatorApiKey, args.eins)
       .catch((err) => {
-        logger.error(err, "error calling primary graphql api");
+        logger.error(err, 'error calling primary graphql api');
         throw err;
       });
 
@@ -118,7 +112,7 @@ const lookupCommand: CommandModule<unknown, LookupCommandArgs> = {
         args.outputFile,
         JSON.stringify(result, null, 2),
       );
-      logger.info(`Wrote CharityNavigator data for ${args.ein} to ${args.outputFile}`);
+      logger.info(`Wrote CharityNavigator data for ${JSON.stringify(args.ein)} to ${JSON.stringify(args.outputFile)}`);
     } else {
       logger.info({ result }, 'CharityNavigator result');
     }
